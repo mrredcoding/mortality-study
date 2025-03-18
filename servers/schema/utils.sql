79badc8d29f13b5ae16752095d6babbb7d53cee0
Gaoul62 <rayan.capaldi-benamou@efrei.net>
2025-03-18 20:19:01 +0100

    Added triggers to utils.sql in schema

--- servers/schema/utils.sql
+++ servers/schema/utils.sql
@@ -32,3 +32,51 @@ $$ LANGUAGE plpgsql;
 
 -- Example:
 SELECT * FROM get_max_mortality_rate(0)
+
+-- Function: Raise an Exception if update on age or gender
+CREATE OR REPLACE FUNCTION prevent_change_gender_age_value()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF (NEW.Age <> OLD.Age OR NEW.Gender <> OLD.Gender) THEN
+        RAISE EXCEPTION 'Trying to change Age or Gender value (Old value : Age=% and Gender=% | New valeur : Age=% and Gender=%).',
+            OLD.Age, OLD.Gender, NEW.Age, NEW.Gender;
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Trigger: Before update execute function
+CREATE TRIGGER check_change_gender_age
+BEFORE UPDATE ON mortality_rate
+FOR EACH ROW EXECUTE PROCEDURE prevent_change_gender_age_value();
+
+-- Function: Raise an Exception if gender isn't Male or Female
+CREATE OR REPLACE FUNCTION prevent_invalid_gender()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF NEW.Gender NOT IN ('Male', 'Female') THEN
+        RAISE EXCEPTION 'Invalid gender value : %, it can only be "Male" or "Female".', NEW.Gender;
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Trigger: Before insert, execute function prevent_invalid_gender()
+CREATE TRIGGER check_gender_before_insert
+BEFORE INSERT ON mortality_rate
+FOR EACH ROW EXECUTE PROCEDURE prevent_invalid_gender();
+
+-- Function: Insert which entry had changes
+CREATE OR REPLACE FUNCTION audit_mortality_rate_changes()
+RETURNS TRIGGER AS $$
+BEGIN
+    INSERT INTO mortality_rate_audit(operation, age, gender)
+    VALUES (TG_OP, COALESCE(NEW.Age, OLD.Age), COALESCE(NEW.Gender, OLD.Gender));
+    RETURN NULL;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Trigger: Before insert, update and delete, execute function mortality_rate_audit_trigger()
+CREATE TRIGGER mortality_rate_audit_trigger
+AFTER INSERT OR UPDATE OR DELETE ON mortality_rate
+FOR EACH ROW EXECUTE PROCEDURE audit_mortality_rate_changes();
