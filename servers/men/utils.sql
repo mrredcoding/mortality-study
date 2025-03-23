76ec1357f631a21fe8700199237a70a716e8ef2b
Gaoul62 <rayan.capaldi-benamou@efrei.net>
2025-03-23 15:44:41 +0100

    Update men utils.sql
    Replace functions by procedures

--- servers/men/utils.sql
+++ servers/men/utils.sql
@@ -1,71 +1,110 @@
--- Function foreign_schema_nodeSchema.get_avg_mortality_rate
+-- Procedures on schema database
+
+-- Procedure foreign_schema_nodeSchema.get_avg_mortality_rate
 -- Retrieve Global Average Mortality Rate depending on the comorbidity
-CREATE OR REPLACE FUNCTION foreign_schema_nodeSchema.get_avg_mortality_rate(cci_level INT)
-RETURNS TABLE(Average_Mortality_Rate FLOAT) AS $$
+CREATE OR REPLACE PROCEDURE foreign_schema_nodeSchema.get_avg_mortality_rate(
+    IN cci_level INT,
+    OUT average_mortality_rate FLOAT
+)
+LANGUAGE plpgsql AS $$
 BEGIN
-    RETURN QUERY EXECUTE format('SELECT AVG(CCI%s) FROM foreign_schema_nodeSchema.Mortality_Rate', cci_level);
+    EXECUTE format('SELECT AVG(CCI%s) FROM foreign_schema_nodeSchema.Mortality_Rate', cci_level)
+    INTO average_mortality_rate;
 END;
-$$ LANGUAGE plpgsql;
+$$;
 
 -- Example:
-SELECT * FROM foreign_schema_nodeSchema.get_avg_mortality_rate(0)
-
--- Function get_avg_mortality_rate
--- Retrieve Average Mortality Rate for men depending on the comorbidity and the age
-CREATE OR REPLACE FUNCTION get_avg_mortality_rate(selected_view VARCHAR(50), cci_level INT)
-RETURNS TABLE(Average_Mortality_Rate FLOAT) AS $$
-BEGIN
-    RETURN QUERY EXECUTE format('SELECT AVG(CCI%s) FROM %s', cci_level, selected_view);
-END;
-$$ LANGUAGE plpgsql;
-
--- Example:
-SELECT * FROM get_avg_mortality_rate('mortality_rate_men_from41to65',0)
+CALL foreign_schema_nodeSchema.get_avg_mortality_rate(0, NULL);
 
 -- Function foreign_schema_nodeSchema.get_avg_life_expectancy
 -- Retrieve Global Average Life Expentancy depending on the comorbidity
-CREATE OR REPLACE FUNCTION foreign_schema_nodeSchema.get_avg_life_expectancy(cci_level INT)
-RETURNS TABLE(Average_Life_Expectancy FLOAT) AS $$
+CREATE OR REPLACE PROCEDURE foreign_schema_nodeSchema.get_avg_life_expectancy(
+    IN cci_level INT,
+    OUT average_life_expectancy FLOAT
+)
+LANGUAGE plpgsql AS $$
 BEGIN
-	RETURN QUERY EXECUTE FORMAT('SELECT AVG(EV_CCI%s) FROM foreign_schema_nodeSchema;Mortality_Rate', cci_level);
+    EXECUTE format('SELECT AVG(EV_CCI%s) FROM foreign_schema_nodeSchema.Mortality_Rate', cci_level)
+    INTO average_life_expectancy;
 END;
-$$ LANGUAGE plpgsql;
+$$;
 
 -- Example:
-SELECT * FROM foreign_schema_nodeSchema.get_avg_life_expectancy(0)
-
--- Function get_avg_life_expectancy
--- Average Life Expentancy for men depending on the comorbidity and the age
-CREATE OR REPLACE FUNCTION get_avg_life_expectancy(selected_view VARCHAR(50), cci_level INT)
-RETURNS TABLE(Average_Life_Expectancy FLOAT) AS $$
-BEGIN
-	RETURN QUERY EXECUTE FORMAT('SELECT AVG(EV_CCI%s) FROM %s', cci_level, selected_view);
-END;
-$$ LANGUAGE plpgsql;
-
--- Example:
-SELECT * FROM get_avg_life_expectancy('mortality_rate_men_from41to65',0)
+CALL foreign_schema_nodeSchema.get_avg_life_expectancy(0, NULL);
 
 -- Function foreign_schema_nodeSchema.get_max_mortality_rate
 -- Retrieve Global Max Mortality Rate depending on the comorbidity
-CREATE OR REPLACE FUNCTION foreign_schema_nodeSchema.get_max_mortality_rate(cci_level INT)
-RETURNS TABLE(Age INT, Mortality_Rate FLOAT) AS $$
+CREATE OR REPLACE PROCEDURE foreign_schema_nodeSchema.get_max_mortality_rate(
+    IN cci_level INT,
+    OUT age INT,
+    OUT mortality_rate FLOAT
+)
+LANGUAGE plpgsql AS $$
 BEGIN
-    RETURN QUERY EXECUTE FORMAT('SELECT Age, CCI%s FROM foreign_schema_nodeSchema.Mortality_Rate ORDER BY CCI%s DESC LIMIT 1', cci_level, cci_level);
+    EXECUTE format(
+        'SELECT Age, CCI%s FROM foreign_schema_nodeSchema.Mortality_Rate ORDER BY CCI%s DESC LIMIT 1', 
+        cci_level, cci_level
+    )
+    INTO age, mortality_rate;
 END;
-$$ LANGUAGE plpgsql;
+$$;
 
 -- Example:
-SELECT * FROM foreign_schema_nodeSchema.get_max_mortality_rate(0)
+CALL foreign_schema_nodeSchema.get_max_mortality_rate(0, NULL, NULL);
+
+-- Procedures on men database
+
+-- Procedure get_avg_mortality_rate
+-- Retrieve Average Mortality Rate for men depending on the comorbidity and the age
+CREATE OR REPLACE PROCEDURE get_avg_mortality_rate(
+    IN selected_view VARCHAR(50),
+    IN cci_level INT,
+    OUT average_mortality_rate FLOAT
+)
+LANGUAGE plpgsql AS $$
+BEGIN
+    EXECUTE format('SELECT AVG(CCI%s) FROM %I', cci_level, selected_view)
+    INTO average_mortality_rate;
+END;
+$$;
+
+-- Example:
+CALL get_avg_mortality_rate('mortality_rate_men_from41to65', 0, NULL);
+
+-- Function get_avg_life_expectancy
+-- Average Life Expentancy for men depending on the comorbidity and the age
+CREATE OR REPLACE PROCEDURE get_avg_life_expectancy(
+    IN selected_view VARCHAR(50),
+    IN cci_level INT,
+    OUT average_life_expectancy FLOAT
+)
+LANGUAGE plpgsql AS $$
+BEGIN
+    EXECUTE format('SELECT AVG(EV_CCI%s) FROM %I', cci_level, selected_view)
+    INTO average_life_expectancy;
+END;
+$$;
+
+-- Example:
+CALL get_avg_life_expectancy('mortality_rate_men_from41to65', 0, NULL);
 
 -- Function get_max_mortality_rate
 -- Retrieve Max Mortality Rate for men depending on the comorbidity and the age
-CREATE OR REPLACE FUNCTION get_max_mortality_rate(selected_view VARCHAR(50), cci_level INT)
-RETURNS TABLE(Age INT, Mortality_Rate FLOAT) AS $$
+CREATE OR REPLACE PROCEDURE get_max_mortality_rate(
+    IN selected_view VARCHAR(50),
+    IN cci_level INT,
+    OUT age INT,
+    OUT mortality_rate FLOAT
+)
+LANGUAGE plpgsql AS $$
 BEGIN
-    RETURN QUERY EXECUTE FORMAT('SELECT Age, CCI%s FROM %s ORDER BY CCI%s DESC LIMIT 1', cci_level, selected_view, cci_level);
+    EXECUTE format(
+        'SELECT Age, CCI%s FROM %I ORDER BY CCI%s DESC LIMIT 1', 
+        cci_level, selected_view, cci_level
+    )
+    INTO age, mortality_rate;
 END;
-$$ LANGUAGE plpgsql;
+$$;
 
 -- Example:
-SELECT * FROM get_max_mortality_rate('mortality_rate_men_from41to65',0)
+CALL get_max_mortality_rate('mortality_rate_men_from41to65', 0, NULL, NULL);
